8d5e7c114fcbb72d17bf3dcc4b1a7645d5a392df
Plawan123 <plawann.24210@obhs.school.nz>
2025-08-25 12:13:32 +1200

    intro

---
+++ 1.1_Plawan_Nilsakorn.py
@@ -0,0 +1,16 @@
+'''
+    author: Plawan NIlsakorn
+    date: 25/08/2025
+    version: 1
+    description: 1.1 assesment - Fitness Tracker
+'''
+
+
+
+
+
+
+#------------------main routine------------------------------
+print("Welcome to Y11 Fitness Trcker!")
+name = str(input("Enter your name: "))
+age = int(input("Enter your age: "))
