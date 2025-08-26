f5b53247ad29687fa99927fa0673dd740bf21281
Plawan123 <plawann.24210@obhs.school.nz>
2025-08-26 14:32:34 +1200

    Update 1.1_Plawan_Nilsakorn.py

--- 1.1_Plawan_Nilsakorn.py
+++ 1.1_Plawan_Nilsakorn.py
@@ -5,13 +5,24 @@
     description: 1.1 assesment - Fitness Tracker
 '''
     
+def fitness():
+        count_push_up = 0
+        for i in range(7):
+            while True:
+                  count_push_up = int(input("How many push up have you done toady: "))
+                  if(count_push_up >= 80):
+                        print("Nice you are strong keep workinghard!")
+                  elif(count_push_up >= 50):
+                        print("You are average keep working hard")
+                  
+                
 
 
 
+fitness()
 
 #------------------main routine------------------------------
 print("Welcome to Y11 Fitness Trcker!")
 name = str(input("Enter your name: "))
 age = int(input("Enter your age: "))
-store = int(input("Push up"
-                 "Pull up"))
+
