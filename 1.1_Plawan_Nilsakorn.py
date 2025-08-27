a71168604c98d0fa9d976214af3964cf6449404c
Plawan123 <plawann.24210@obhs.school.nz>
2025-08-28 10:19:46 +1200

    Update 1.1_Plawan_Nilsakorn.py

--- 1.1_Plawan_Nilsakorn.py
+++ 1.1_Plawan_Nilsakorn.py
@@ -15,10 +15,11 @@
 #------------------------------------
 def fitness():
     total = 0  # Set total push up of whole week to 0
+    total_2()
     count_push_up = 0 # Set push up in each day to 0
     each_day = []  # List that will store how many push up doing in a day
-    each_day.append(count_push_up) 
-    for i in range(1,8):  # If the input not error the loop will go for 8 times
+    each_day.append(count_push_up)
+    for count_push_up in range(1,8):  # If the input not error the loop will go for 8 times
         while True:  # Check how many push up been doing
             try:  
                 count_push_up = int(input("How many push up have you done toady: "))
@@ -36,13 +37,14 @@ def fitness():
                     print("Are you kidding me?")
                      
                 total += count_push_up  # Add all amount of push up that store in list together to get a total push up       
-                    
+                total_2 = max   
+
                 break
                 
             except:  # If user enter alphabet it will print this
                 print("Error, please enter with a number")                  
     
-    print(f"Nice {name} you did {total} push up for this week")
+    print(f"Nice {name} you did {total} push up for this week {total_2}")
 
 
 #---------------------main routine---------------------------
