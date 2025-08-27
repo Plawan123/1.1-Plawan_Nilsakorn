cda617dd827436e15e23e6431f28dd41a5551d20
Plawan123 <plawann.24210@obhs.school.nz>
2025-08-28 09:28:28 +1200

    Update 1.1_Plawan_Nilsakorn.py
    Use while loop to force user to enter name between 2 - 20 letters and I also add some comment in the code

--- 1.1_Plawan_Nilsakorn.py
+++ 1.1_Plawan_Nilsakorn.py
@@ -6,19 +6,21 @@
 '''
 
 
-
-
-
 #-----------------function-----------------------------------
 #------------------------------------
-#This function is for check how many
-#they can do and how strong they are
+# To check how many push up user been doing
+# and tell them how stong they are. 
+# Also add all total push up that user been
+# doing for 7 days or a week
 #------------------------------------
 def fitness():
-    count_push_up = 0
-    for i in range(1,8):
-        while True:
-            try:
+    total = 0  # Set total push up of whole week to 0
+    count_push_up = 0 # Set push up in each day to 0
+    each_day = []  # List that will store how many push up doing in a day
+    each_day.append(count_push_up) 
+    for i in range(1,8):  # If the input not error the loop will go for 8 times
+        while True:  # Check how many push up been doing
+            try:  
                 count_push_up = int(input("How many push up have you done toady: "))
                 if(count_push_up >= 80):
                     print("Nice you are very strong keep workinghard!")
@@ -32,17 +34,37 @@ def fitness():
                     print("Come on you can do better than this!")
                 else:
                     print("Are you kidding me?")
-
+                     
+                total += count_push_up  # Add all amount of push up that store in list together to get a total push up       
+                    
                 break
                 
-            except:
+            except:  # If user enter alphabet it will print this
                 print("Error, please enter with a number")                  
-                
+    
+    print(f"Nice {name} you did {total} push up for this week")
 
 
 #---------------------main routine---------------------------
-print("Welcome to Y11 Fitness Trcker!")  #greeting!!
-name = str(input("Enter your name: "))
-age = int(input("Enter your age: "))
+Min = 2  # Minimum of letter for user name
+Max = 20  # Maximum of letter for user name
+while True:  #Check if user name between Min and Max
+    name = str(input("Enter your name: "))
+    if(len(name) > Min and len(name) < Max and name.isalpha): 
+        print(f"Hi {name}! Welcome to Y11 Fitness Trcker!")  #greeting!!
+        break
+    else:
+        print("Please try again")
+
+
+while True: 
+    try:
+        age = int(input("Enter your age betwee 14-17: "))
+        if(14 <= age <= 17):
+            break
+        else:
+            print("Sorry, age must be between 14 and 17.")
+    except:
+        print("Please enter with a number.")
 
 fitness()
